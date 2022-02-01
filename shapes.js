c7ef3161fbfa296d9520d0b1dbf76a7cdc7ac379
vbirnberg <98720631+vbirnberg@users.noreply.github.com>
2022-02-01 13:44:38 -0600

    Add files via upload

---
+++ shapes.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background (52, 194, 237);
+
+}
+
+function draw() {
+  background(52, 194, 237);
+  fill (173, 124, 87)
+  circle (300, 300, 100);
+
+  fill (105, 36, 40)
+  rect (230, 230, 50, 50)
+  fill (105, 36, 40)
+  rect (330, 230, 50, 50)
+  circle (305,330, 30)
+  circle (275, 300, 20)
+  circle (320, 300, 20)
+ ellipse (250, 445, 300, 200)
+
+}
