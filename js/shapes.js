c66986682f0d1d086b663e8c20bb959c1ca73cb5
vbirnberg <98720631+vbirnberg@users.noreply.github.com>
2022-02-08 17:22:40 -0600

    Update shapes.js

--- js/shapes.js
+++ js/shapes.js
@@ -5,17 +5,38 @@ function setup() {
 }
 
 function draw() {
-  background(52, 194, 237);
+  if(mouseIsPressed == true){
+    background(200, 100, 50);
+  }else{
+    background(52, 194, 237);
+  }
+
   fill (173, 124, 87)
   circle (300, 300, 100);
 
   fill (105, 36, 40)
   rect (230, 230, 50, 50)
+
   fill (105, 36, 40)
   rect (330, 230, 50, 50)
-  circle (305,330, 30)
+
+  if(keyIsPressed == true){
+    strokeWeight(10);
+    //random requires at least one argument. Will return a reandom
+    //value between the ranges
+    rect(305,330, 10,5)
+
+  }else{
+    strokeWeight(4);
+    stroke(20, 100, 66);
+    circle (305,330, 30)
+  }
+
+
   circle (275, 300, 20)
   circle (320, 300, 20)
+
  ellipse (250, 445, 300, 200)
 
 }
+
